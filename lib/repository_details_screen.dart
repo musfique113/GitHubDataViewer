84f8252d9d9500a6c64a957d0597802f0739407e
Mahfujur Rahman Musfique <musfique113@gmail.com>
2023-06-21 19:57:26 +0600

    update in UI

--- lib/repository_details_screen.dart
+++ lib/repository_details_screen.dart
@@ -12,16 +12,52 @@ class RepositoryDetails extends StatelessWidget {
       ),
       body: Padding(
         padding: EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text('Description: ${repository['description'] ?? 'N/A'}'),
-            Text('Stars: ${repository['stargazers_count']}'),
-            Text('Forks: ${repository['forks_count']}'),
-            Text('Language: ${repository['language'] ?? 'N/A'}'),
-            Text('Last Updated: ${repository['updated_at']}'),
-          ],
-        ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Description: ${repository['description'] ?? 'N/A'}',
+                style: TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+              SizedBox(height: 10.0),
+              Text(
+                'Stars: ${repository['stargazers_count']}',
+                style: TextStyle(
+                  fontSize: 16.0,
+                  color: Colors.blueGrey,
+                ),
+              ),
+              SizedBox(height: 10.0),
+              Text(
+                'Forks: ${repository['forks_count']}',
+                style: TextStyle(
+                  fontSize: 16.0,
+                  color: Colors.blueGrey,
+                ),
+              ),
+              SizedBox(height: 10.0),
+              Text(
+                'Language: ${repository['language'] ?? 'N/A'}',
+                style: TextStyle(
+                  fontSize: 16.0,
+                  color: Colors.blueGrey,
+                ),
+              ),
+              SizedBox(height: 10.0),
+              Text(
+                'Last Updated: ${repository['updated_at']}',
+                style: TextStyle(
+                  fontSize: 16.0,
+                  color: Colors.blueGrey,
+                ),
+              ),
+            ],
+          ),
+
       ),
     );
   }
